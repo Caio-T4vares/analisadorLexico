953842ce51daa6572396fcf3822dbc64da3fe1f1
DanielVII <danieldantaswa@gmail.com>
2023-12-21 08:50:53 -0300

    View dos dados

--- parser.h
+++ parser.h
@@ -1,10 +1,12 @@
 #include <FlexLexer.h>
 #include "tokens.h"
-
-#include <unordered_map>
+#include <deque>
 #include <string>
+#include <unordered_map>
+
 using std::unordered_map;
 using std::string;
+using std::deque;
 
 class Parser
 {
@@ -12,8 +14,36 @@ private:
 	yyFlexLexer scanner;
 	int lookahead;
 	unordered_map<string, tag> id_table;
+	void AddInTodosTokens(string token, string nomeId);
+	bool AddInIdTable(string s, tag t);
 
-	void AddInIdTable(string s, tag t);
+	deque<string> todosTokensList;
+    deque<string> cardinalList;
+    deque<string> idClassesList;
+    deque<string> propsList;
+    deque<string> namesList;
+    deque<string> typesList;
+	int cardinalQuant=0;
+	int idQuant=0;
+	int propQuant=0;
+	int nameQuant=0;
+	int typeQuant=0;
+    int symbolQuant =0;
+    int someQuant=0;
+    int allQuant=0;
+    int valueQuant=0;
+    int minQuant=0;
+    int maxQuant=0;
+    int exactlyQuant=0;
+    int thatQuant=0;
+    int notTokenQuant=0;
+    int andTokenQuant=0;
+    int orTokenQuant=0;
+    int IndividualsQuant=0;
+    int ClassQuant=0;
+    int EquivalentToQuant=0;
+    int SubClassOfQuant=0;
+    int DisjointClassesQuant=0;
 	
 public:
 	Parser();
